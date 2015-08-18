dd138eb66adde3f78fb19968a4061a63175aa192
Dan Van Atta <dhvatta@gmail.com>
2015-08-17 23:33:42 -0700

    Fix artifact names. Add mac implementation start.

--- .travis/buildMacDmg.sh
+++ .travis/buildMacDmg.sh
@@ -2,4 +2,15 @@
 
 set -ev
 
-echo "does not yet do anything"
+DMG_FILE=artifacts/triplea_$TRAVIS_TAG_mac.dmg
+APP_FILE=release/Triplea.app
+hdiutil detach /Volumes/TripleA
+
+
+TMP_DIR=release.mac
+
+mkdir -p $TMP_DIR/Contents/MacOS $TMP_DIR/Contents/Resources/Java
+echo "copy file="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Resources/MacOS/JavaApplicationStub todir={app.file}/Contents/MacOS/"
+
+
+
